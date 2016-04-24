bf74f63716839b727aac6aaf51e0c6b5cca0b86b
Justin Yoo <justin.yoo@aliencube.com>
2016-04-24 21:24:19 +1000

    Initial source code commit

---
+++ src/AspNetCoreDevOpsSample.PsScripts/Login-AzureRmServicePrincipal.ps1
@@ -0,0 +1,36 @@
+﻿# Login to Azure RM with service principal.
+# If both ClientSecret and SecureClientSecret are provided, ClientSecret will be ignored.
+function Login-AzureRmServicePrincipal([string]$TenantId, [string]$ClientId, [string]$ClientSecret, [System.Security.SecureString]$SecureClientSecret)
+{
+	if ([String]::IsNullOrWhiteSpace($TenantId))
+	{
+		Write-Host "TenantId not provided" -ForegroundColor White -BackgroundColor Red
+		return
+	}
+
+	if ([String]::IsNullOrWhiteSpace($ClientId))
+	{
+		Write-Host "ClientId not provided" -ForegroundColor White -BackgroundColor Red
+		return
+	}
+
+	if ([String]::IsNullOrWhiteSpace($ClientSecret) -and $SecureClientSecret -eq $null)
+	{
+		Write-Host "Neither ClientSecret nor SecureClientSecret provided" -ForegroundColor White -BackgroundColor Red
+		return
+	}
+
+	if (![String]::IsNullOrWhiteSpace($ClientSecret))
+	{
+		$password = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
+	}
+
+	if ($SecureClientSecret -ne $null)
+	{
+		$password = $SecureClientSecret
+	}
+
+	$credential = New-Object System.Management.Automation.PSCredential ($ClientId, $password)
+
+	Login-AzureRmAccount -ServicePrincipal -Tenant $TenantId -Credential $credential
+}
---
+++ src/AspNetCoreDevOpsSample.PsScripts/New-AzureRmServicePrincipal.ps1
@@ -0,0 +1,32 @@
+﻿# Creates a new service principal.
+function New-AzureRmServicePrincipal([string]$SubscriptionId, [string]$ClientId, [string]$ResourceGroupName)
+{
+	if ([String]::IsNullOrWhiteSpace($ClientId))
+	{
+		Write-Host "ClientId not provided" -ForegroundColor White -BackgroundColor Red
+		return
+	}
+
+	Login-AzureRmAccount
+
+	# If subscriptionId is not provided, just use the default subscriptionId.
+	if (![String]::IsNullOrWhiteSpace($SubscriptionId))
+	{
+		Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+	}
+
+	# If resource group name is provided, servicce principal is only scoped to the resource group;
+	# otherwise it is scoped to the subscriptionId.
+	if (![String]::IsNullOrWhiteSpace($ResourceGroupName))
+	{
+		New-AzureRmRoleAssignment -ServicePrincipalName $ClientId -RoleDefinitionName "Contributor" -ResourceGroupName $ResourceGroupName
+
+		Write-Host "Service Principal created to $ResourceGroupName" -ForegroundColor Green
+	}
+	else
+	{
+		New-AzureRmRoleAssignment -ServicePrincipalName $ClientId -RoleDefinitionName "Contributor"
+
+		Write-Host "Service Principal created" -ForegroundColor Green
+	}
+}
