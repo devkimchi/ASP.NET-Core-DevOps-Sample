184f14ad94a2ca7d99a3b67d52ae76e4539b84f9
Justin Yoo <justin.yoo@aliencube.com>
2016-04-27 13:33:33 +1000

    Fix reference files' location

--- test/AspNetCoreDevOpsSample.WebApp.Tests/spec/javascripts/HomeIndexTest.js
+++ test/AspNetCoreDevOpsSample.WebApp.Tests/spec/javascripts/HomeIndexTest.js
@@ -1,7 +1,7 @@
-﻿/// <reference path="../node_modules/jasmine/lib/jasmine.js"/>
-/// <reference path="../node_modules/jquery/dist/jquery.js"/>
-/// <reference path="../node_modules/jasmine-jquery/lib/jasmine-jquery.js"/>
-/// <reference path="../../../src/AspNetCoreDevOpsSample.WebApp/wwwroot/js/home.js"/>
+﻿/// <reference path="../../node_modules/jasmine/lib/jasmine.js"/>
+/// <reference path="../../node_modules/jquery/dist/jquery.js"/>
+/// <reference path="../../node_modules/jasmine-jquery/lib/jasmine-jquery.js"/>
+/// <reference path="../../../../src/AspNetCoreDevOpsSample.WebApp/wwwroot/js/home.js"/>
 
 describe("When the 'learn more' button is clicked", function () {
     var msgA = "Learn More";
